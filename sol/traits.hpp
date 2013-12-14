53416825033929f9ecc48e6fd65fdc58226aa4ba
Rapptz <rapptz@gmail.com>
2013-12-14 04:42:53 -0500

    Change tabs to spaces

--- sol/traits.hpp
+++ sol/traits.hpp
@@ -73,58 +73,58 @@ struct function_traits;
 
 template<typename T, typename R, typename... Args>
 struct function_traits<R(T::*)(Args...)> {
-	static const std::size_t arity = sizeof...(Args);
-	static const bool is_member_function = true;
-	typedef std::tuple<Args...> arg_tuple_type;
-	typedef types<Args...> args_type;
-	typedef R(T::* function_pointer_type)(Args...);
-	typedef typename std::remove_pointer<function_pointer_type>::type function_type;
-	typedef R(*free_function_pointer_type)(Args...);
-	typedef R return_type;
-	template<std::size_t i>
-	using arg = typename std::tuple_element<i, arg_tuple_type>::type;
+    static const std::size_t arity = sizeof...(Args);
+    static const bool is_member_function = true;
+    typedef std::tuple<Args...> arg_tuple_type;
+    typedef types<Args...> args_type;
+    typedef R(T::* function_pointer_type)(Args...);
+    typedef typename std::remove_pointer<function_pointer_type>::type function_type;
+    typedef R(*free_function_pointer_type)(Args...);
+    typedef R return_type;
+    template<std::size_t i>
+    using arg = typename std::tuple_element<i, arg_tuple_type>::type;
 };
 
 template<typename T, typename R, typename... Args>
 struct function_traits<R(T::*)(Args...) const> {
-	static const std::size_t arity = sizeof...(Args);
-	static const bool is_member_function = true;
-	typedef std::tuple<Args...> arg_tuple_type;
-	typedef types<Args...> args_type;
-	typedef R(T::* function_pointer_type)(Args...);
-	typedef typename std::remove_pointer<function_pointer_type>::type function_type;
-	typedef R(*free_function_pointer_type)(Args...);
-	typedef R return_type;
-	template<std::size_t i>
-	using arg = typename std::tuple_element<i, arg_tuple_type>::type;
+    static const std::size_t arity = sizeof...(Args);
+    static const bool is_member_function = true;
+    typedef std::tuple<Args...> arg_tuple_type;
+    typedef types<Args...> args_type;
+    typedef R(T::* function_pointer_type)(Args...);
+    typedef typename std::remove_pointer<function_pointer_type>::type function_type;
+    typedef R(*free_function_pointer_type)(Args...);
+    typedef R return_type;
+    template<std::size_t i>
+    using arg = typename std::tuple_element<i, arg_tuple_type>::type;
 };
 
 template<typename R, typename... Args>
 struct function_traits<R(Args...)> {
-	static const std::size_t arity = sizeof...(Args);
-	static const bool is_member_function = false;
-	typedef std::tuple<Args...> arg_tuple_type;
-	typedef types<Args...> args_type;
-	typedef R(function_type)(Args...);
-	typedef R(*function_pointer_type)(Args...);
-	typedef R(*free_function_pointer_type)(Args...);
-	typedef R return_type;
-	template<std::size_t i>
-	using arg = typename std::tuple_element<i, arg_tuple_type>::type;
+    static const std::size_t arity = sizeof...(Args);
+    static const bool is_member_function = false;
+    typedef std::tuple<Args...> arg_tuple_type;
+    typedef types<Args...> args_type;
+    typedef R(function_type)(Args...);
+    typedef R(*function_pointer_type)(Args...);
+    typedef R(*free_function_pointer_type)(Args...);
+    typedef R return_type;
+    template<std::size_t i>
+    using arg = typename std::tuple_element<i, arg_tuple_type>::type;
 };
 
 template<typename R, typename... Args>
 struct function_traits<R(*)(Args...)> {
-	static const std::size_t arity = sizeof...(Args);
-	static const bool is_member_function = false;
-	typedef std::tuple<Args...> arg_tuple_type;
-	typedef types<Args...> args_type;
-	typedef R(function_type)(Args...);
-	typedef R(*function_pointer_type)(Args...);
-	typedef R(*free_function_pointer_type)(Args...);
-	typedef R return_type;
-	template<std::size_t i>
-	using arg = typename std::tuple_element<i, arg_tuple_type>::type;
+    static const std::size_t arity = sizeof...(Args);
+    static const bool is_member_function = false;
+    typedef std::tuple<Args...> arg_tuple_type;
+    typedef types<Args...> args_type;
+    typedef R(function_type)(Args...);
+    typedef R(*function_pointer_type)(Args...);
+    typedef R(*free_function_pointer_type)(Args...);
+    typedef R return_type;
+    template<std::size_t i>
+    using arg = typename std::tuple_element<i, arg_tuple_type>::type;
 };
 } // sol
 
