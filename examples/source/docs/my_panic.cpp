58149922e3c1479b7e92e5a1709e10c21435509b
Marco Melorio <m.melorio@icloud.com>
2019-07-08 19:37:19 +0200

    Fix typo in exception documentation

--- examples/source/docs/my_panic.cpp
+++ examples/source/docs/my_panic.cpp
@@ -14,10 +14,10 @@ inline void my_panic(sol::optional<std::string> maybe_msg) {
 int main (int, char*[]) {
 	sol::state lua(sol::c_call<decltype(&my_panic), &my_panic>);
 	// or, if you already have a lua_State* L
-	// lua_atpanic( L, sol::c_call<decltype(&my_panic)>, &my_panic> );
+	// lua_atpanic( L, sol::c_call<decltype(&my_panic), &my_panic> );
 	// or, with state/state_view:
 	// sol::state_view lua(L);
-	// lua.set_panic( sol::c_call<decltype(&my_panic)>, &my_panic> );
+	// lua.set_panic( sol::c_call<decltype(&my_panic), &my_panic> );
 
 	// uncomment the below to see
 	//lua.script("boom_goes.the_dynamite");
