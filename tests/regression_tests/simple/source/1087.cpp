9c4e2d10948c72d7d63dcc77eb383715c185392f
ThePhD <phdofthehouse@gmail.com>
2020-12-17 23:37:00 -0500

    Add regression test for #1087

---
+++ tests/regression_tests/simple/source/1087.cpp
@@ -0,0 +1,27 @@
+#include <sol/sol.hpp>
+
+inline namespace sol2_regression_test_1087 {
+	struct MyStruct {
+		int prop = 10;
+	};
+} // namespace sol2_regression_test_1087
+
+int regression_1087() {
+	sol::state lua;
+	lua.open_libraries(sol::lib::base,
+	     sol::lib::package,
+	     sol::lib::coroutine,
+	     sol::lib::string,
+	     sol::lib::os,
+	     sol::lib::math,
+	     sol::lib::table,
+	     sol::lib::io,
+	     sol::lib::debug);
+	lua.new_usertype<MyStruct>("MyStruct", "prop", sol::readonly_property(&MyStruct::prop));
+	sol::optional<sol::error> maybe_error = lua.safe_script("local s = MyStruct.new(); s.prop = 20", sol::script_pass_on_error);
+	if (maybe_error.has_value()) {
+		// good: cannot write to this property
+		return 0;
+	}
+	return 1;
+}
--- tests/regression_tests/simple/source/main.cpp
+++ tests/regression_tests/simple/source/main.cpp
@@ -4,6 +4,7 @@ extern int regression_1000();
 extern int regression_1008();
 extern int regression_1067();
 extern int regression_1072();
+extern int regression_1087();
 
 using f_ptr = int();
 
@@ -17,7 +18,7 @@ int trampoline(f_ptr* f) {
 }
 
 int main(int, char*[]) {
-	f_ptr* regressions[] = { &regression_1008, &regression_1000, &regression_1067, &regression_1072 };
+	f_ptr* regressions[] = { &regression_1008, &regression_1000, &regression_1067, &regression_1072, &regression_1087 };
 	const int sizeof_regressions = sizeof(regressions) / sizeof(regressions[0]);
 	int r = 0;
 	for (std::size_t i = 0; i < sizeof_regressions; ++i) {
