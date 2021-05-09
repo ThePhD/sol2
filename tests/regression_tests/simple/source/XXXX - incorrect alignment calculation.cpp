491e77fe0baa40c155d67b6a980333afb36e17f3
Smertig <akaraevz@mail.ru>
2021-05-09 13:07:01 +0300

    Add test with misaligned allocation

---
+++ tests/regression_tests/simple/source/XXXX - incorrect alignment calculation.cpp
@@ -0,0 +1,32 @@
+#include <sol/sol.hpp>
+
+#if SOL_TESTS_SIZEOF_VOID_P == 4
+
+inline namespace sol2_regression_test_XXXX {
+	struct Test {
+		std::uint64_t dummy;
+	};
+} // namespace sol2_regression_test_XXXX
+
+unsigned int regression_XXXX() {
+	sol::state lua;
+
+	static_assert(sizeof(Test) == 8);
+	static_assert(alignof(Test) == 8);
+	static_assert(sizeof(Test*) == 4);
+	static_assert(alignof(Test*) == 4);
+
+	/// [sol2] An error occurred and panic has been invoked: aligned allocation of userdata block (data section) for 'sol2_regression_test_XXXX::Test' failed
+	/// Note: may not panic depending on alignment of local variable `alignment_shim` in sol::detail::aligned_space_for
+	lua["test"] = Test {};
+
+	return 0;
+}
+
+#else
+
+unsigned int regression_XXXX() {
+	return 0;
+}
+
+#endif
