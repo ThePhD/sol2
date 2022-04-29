2ece368db4801880b20fcb269646d4d80a59756a
Immortale <immortalety@mail.ru>
2022-04-29 17:53:31 +0300

    Fixed exception condition

--- include/sol/stack_guard.hpp
+++ include/sol/stack_guard.hpp
@@ -31,7 +31,7 @@
 namespace sol {
 	namespace detail {
 		inline void stack_fail(int, int) {
-#if SOL_IS_OFF(SOL_EXCEPTIONS_I_)
+#if SOL_IS_ON(SOL_EXCEPTIONS_I_)
 			throw error(detail::direct_error, "imbalanced stack after operation finish");
 #else
 			// Lol, what do you want, an error printout? :3c
