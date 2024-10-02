9f8041323d2865e302c181a9d79d1e8be11c92e2
deadlocklogic <57906342+deadlocklogic@users.noreply.github.com>
2024-10-02 14:04:17 +0300

    usertype_container.hpp: fix bugs introduced by previous refactoring

--- include/sol/usertype_container.hpp
+++ include/sol/usertype_container.hpp
@@ -1188,8 +1188,8 @@ namespace sol {
 			template <bool ip>
 			static int next_associative(std::true_type, lua_State* L_) {
 				iter& i = stack::unqualified_get<user<iter>>(L_, 1);
-				auto& it = i.it;
-				auto& end = i.end;
+				auto& it = i.it();
+				auto& end = i.sen();
 				if (it == end) {
 					return stack::push(L_, lua_nil);
 				}
