c9f7c084cfbdffc58a6d974e186e369d571c0bf7
Vas Crabb <vas@vastheman.com>
2024-11-18 06:02:09 +1100

    Simplify sol::optional<T&>::emplace

--- include/sol/optional_implementation.hpp
+++ include/sol/optional_implementation.hpp
@@ -2186,12 +2186,8 @@ namespace sol {
 		/// one.
 		///
 		/// \group emplace
-		template <class... Args>
-		T& emplace(Args&&... args) noexcept {
-			static_assert(std::is_constructible<T, Args&&...>::value, "T must be constructible with Args");
-
-			*this = nullopt;
-			new (static_cast<void*>(this)) optional(std::in_place, std::forward<Args>(args)...);
+		T& emplace(T& arg) noexcept {
+			m_value = &arg;
 			return **this;
 		}
 
